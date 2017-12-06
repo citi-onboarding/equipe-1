fbd2bee9cf5adc45594f94c3827854120f1614a0
gabrielaleal <gabriela.leal@hotmail.com>
2017-12-06 12:53:34 -0300

    issue #1 e issue #2

--- equipe1/core/models.py
+++ equipe1/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nome = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, primary_key=True)
+    senha = models.CharField(min_length=6)
+    telefone = models.CharField(min_length=11)
+    casas = models.ManyToManyField(Casa)
+
+class Casa(models.Model):
+    titulo = models.CharField(max_length=100)
+    valor = models.FloatField()
+    endereco = models.CharField(max_length=140)
+    data_adicao = models.DateField(auto_now=True) #vai ter argumento?
+    foto = models.FileField(upload_to='') #botar o caminho da pasta onde vao ficar as imagens
+    usuario = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+    codigo = models.AutoField(primary_key=True)
--- equipe1/core/views.py
+++ equipe1/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def signup:
+    if(request.method == POST):
+        form = UserCreationForm
